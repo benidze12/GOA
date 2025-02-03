5da2c4698c3e24af4a1814cc623d245fe05b6f58
benidze12 <sababenidze12@gmail.com>
2025-02-03 18:01:03 +0400

    commit

---
+++ day17/classwork/cw01.py
@@ -0,0 +1,14 @@
+#1) for loop-ში გამოიტანეთ 950-დან 1000-ის ჩათვლით ლუწი რიცხვები.
+#2) for loop-ში გამოიტანეთ თქვენი სახელი 100-ჯერ  
+#3) for loop-ში გამოიტანე "Goa Is the best" 1000-ჯერ
+#4) for loop-ში გამოიტანე რიცხვები 100-დან 200-მდე     
+
+
+for i in range(950, 1001, 2):
+      print(i)
+for i in range(100):
+      print("saba")
+for i in range(1000):
+      print("Goa Is the best")
+for i in range(100, 200):
+      print(i)
