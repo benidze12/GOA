890d3033565e86507441a9c104d26a5cf4efb2ea
benidze12 <sababenidze12@gmail.com>
2025-02-16 23:00:01 +0400

    commit

---
+++ day19/classwork/cw02.py
@@ -0,0 +1,18 @@
+#3) თქვენი სიტყვებით ახსენით განსხვავება while და for ციკლებს შორის
+
+#4) დაბეჭდეთ "GOA BEST!!!" 50-ჯერ, ორივე ციკლით შეასრულეთ ეს დავალება
+for i in range(50):
+    print("goa is the best")
+
+    i=0
+    while i<50:
+        print("GOA BEST!!!")
+        i +=1
+
+     #5) გამოიყენეთ while loop_ი რომ, დაპრინტოთ ლუწი რიცხვები  1-დან 20-მდე   
+
+    i=1
+    while "total" < 20:
+        print("total")
+        "total" += 2
+
