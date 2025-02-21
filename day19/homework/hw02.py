acd5ed19fb0fc4b13acc012a49e1529eb78196d8
benidze12 <sababenidze12@gmail.com>
2025-02-21 16:04:11 +0400

    commit

---
+++ day19/homework/hw02.py
@@ -0,0 +1,7 @@
+#2)დაბეჭდეთ "GOA BEST!!!" 100-ჯერ, ორივე ციკლით შეასრულეთ ეს დავალება
+for i in range(100):
+    print("GOA BEST!!!")
+    i=0
+    while i<100:
+     print("GOA BEST!!!")
+    i +=1
