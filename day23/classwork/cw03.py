1894d1bd3734c6a5f56bad12fab71987f894c191
benidze12 <sababenidze12@gmail.com>
2025-03-23 11:52:46 +0400

    commit

---
+++ day23/classwork/cw03.py
@@ -0,0 +1,6 @@
+
+#3)მიწარმოეთ რამდენიმე სიტყვა  HELLOWEN იდან 
+
+num = "hellowen"
+print("hello" "won" "now" "low" "lol" "new" "owl" "one" "no")
+       # 0      1     2     3     4     5     6     7    8
---
+++ day23/classwork/cw04.py
@@ -0,0 +1,7 @@
+#4) შეაერთე სიტყვა რომ გამოვიდეს GOA IS THE BEST ACADEMY
+#["GOA" , "MAGARIA", "NOVATORI","IS","THE", "VIRGIN", "CHAD" ,"BEST" ,"ACADEMY"
+
+goa = ["goa", "magaria", "novatori", "is", "the","virgin", "chad", "best", "academy"]
+        # 0      1            2        3     4      5         6      7        8
+
+print(goa[0] + goa[3] + goa[4] + goa[7] + goa[8])
